63ecd1186965c1ecf52cc7518325e73d00306943
Daniyal Atiq <146195429+DaniyalAtiq9@users.noreply.github.com>
2024-09-25 21:07:00 +0500

    Update climbStairs.py

--- climbStairs.py
+++ climbStairs.py
@@ -9,20 +9,28 @@
 # 1. 1 step + 1 step + 1 step
 # 2. 1 step + 2 steps
 # 3. 2 steps + 1 step
-# I have taken a custom input for no. of stairs.
+# I have taken a custom input for no. of stairs. This is a dynamic programming problem, and the hint here is fibonacci sequence.
+# Try to draw a diagram of stairs to better understand it.
 def climbStairs(n):
+    # if no. of stairs = 1, then only 1 combination to climb them
     if n == 1:
         return 1
+    # if no. of stairs = 2, then only 2 combination to climb them
     elif n == 2:
         return 2
+    # Initializing array with zero entries of size n
     arr = [0] * (n + 1)
+    # We know the combinations to climb 1 and 2 stairs.
     arr[1] = 1
     arr[2] = 2
+    # The no. of combinations to climb let's say 3 stairs is equal to the sum of number of combinations present to climb 1
+    # stair hence  arr[i - 1] and no. of combinations available to climb 2 stairs hence arr[i - 2], this is true for *n* no.
+    # of stairs
     for i in range(3,n + 1):
         arr[i] = arr[i - 1] + arr[i - 2]
     return arr[n]
 
-
+# Main function written for convenience
 if __name__ == "__main__":
     n = int(input("Enter number of stairs: "))
     result = climbStairs(n)
