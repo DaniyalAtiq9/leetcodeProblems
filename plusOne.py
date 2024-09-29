1c9af63bcfcb18d14b73df35507a1bb119e3f9d8
Daniyal Atiq <146195429+DaniyalAtiq9@users.noreply.github.com>
2024-09-30 01:16:35 +0500

    Update plusOne.py

--- plusOne.py
+++ plusOne.py
@@ -1,12 +1,7 @@
-# In this section, I solved LeetCode # 66. Plus One. The
-# easiest problem so far. The description reads as:
-# You are given a large integer represented as an integer array
-# digits, where each digits[i] is the ith digit of the integer.
-# The digits are ordered from most significant to least 
-# significant in left-to-right order. The large integer does
-# not contain any leading 0's.
-# Increment the large integer by one and return the resulting 
-# array of digits.
+# In this section, I solved LeetCode # 66. "Plus One". The description reads as:
+# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+# The digits are ordered from most significant to least  significant in left-to-right order. The large integer does
+# not contain any leading 0's. Increment the large integer by one and return the resulting  array of digits.
 # Example:
 # Input: digits = [1,2,3]
 # Output: [1,2,4]
@@ -36,14 +31,12 @@ def plusOne(digits):
     # beginning of array. We can do this by list concatenation
     return [1] + digits
     
-# For convenience, I have added a main function so you first
-# test on your own IDE and then proceed to LeetCode
+# For convenience, I have added a main function so you first test on your own IDE and then proceed to LeetCode
 if __name__ == "__main__":
     #num = [4,3,2,1]
     #num = [1,2,3]
     num = [9]
     result = plusOne(num)
     print(result)
-# The time complexity of this code is O(n) since the loop
-# n times and space complexity is 0(1), since no extra space
+# The time complexity of this code is O(n) since the loop n times and space complexity is 0(1), since no extra space
 # is used.
